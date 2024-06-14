03a4c0154cf10414b11b3523f730c0edeac7f431
Priyanka <148324851+hpriyankaa@users.noreply.github.com>
2024-06-14 14:11:37 +0530

    hardcoded api key

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from typing import Final
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
-TOKEN: Final = '7203979984:AAFDLD5N7R-K0F352tDjfLyBcLHddVqVNz8'
+TOKEN: Final = 'api_key'
 BOT_USERNAME: Final = 'caketalesbot'
 
 
